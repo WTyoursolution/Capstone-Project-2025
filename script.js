b710358dc57c1bb381eae54a0f091799faeaf97c
Keyla Welcome <kcuddiie@gmail.com>
2025-01-22 18:09:12 -0600

    Capstone-2025

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const projects = [
-  { name: "Driveway Boys", description: "A sleek and responsive website designed for a driveway services business, highlighting offerings and contact information.", githubLink: "<a href=`https://wtyoursolution.github.io/Drivewayboys-Website/`>Click Here</a>" },
+  { name: "Driveway Boys", description: "A sleek and responsive website designed for a driveway services business, highlighting offerings and contact information.", githubLink: "https://wtyoursolution.github.io/Drivewayboys-Website/" },
 
-  { name: "TinDog Website", description: "A fun, responsive landing page for a fictional dog dating app, combining playful design with modern web features. Created as a project in class.", githubLink: "<a href=`https://wtyoursolution.github.io/Tindog-Page/`>Click Here</a>" },
+  { name: "TinDog Website", description: "A fun, responsive landing page for a fictional dog dating app, combining playful design with modern web features. Created as a project in class.", githubLink: "https://wtyoursolution.github.io/Tindog-Page/" },
 
-  { name: "Business Website", description: "Professional business website with a clean layout and responsive design to showcase services and company information", githubLink: "<a href=`https://wtyoursolution.github.io/BusinessWebsite/`>Click Here</a>"},
+  { name: "Business Website", description: "Professional business website with a clean layout and responsive design to showcase services and company information", githubLink: "https://wtyoursolution.github.io/BusinessWebsite/"},
 ];
 let websites = document.getElementById("websites");
 
@@ -19,7 +19,7 @@ for (let i = 0; i < projects.length; i++) {
       <h5 class="card-title">Project</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary">${project.name}</h6>
       <p class="card-text">${project.description}</p>
-      <a href="#" class="card-link">${project.githubLink}</a>
+      <a href="${project.githubLink}" class="card-link">Click Here</a>
             <div>
         </div>
   </div>
